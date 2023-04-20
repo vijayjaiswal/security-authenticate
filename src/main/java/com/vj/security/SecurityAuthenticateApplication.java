0dd206542794d59c59e2a9dbe635bab52db94cd4
Vijay Jaiswal <vijay.jaiswal@qio.io>
2023-04-20 08:40:38 +0530

    Adding Inital working of Bearer Authentication with Postgres

---
+++ src/main/java/com/vj/security/SecurityAuthenticateApplication.java
@@ -0,0 +1,12 @@
+package com.vj.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAuthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAuthenticateApplication.class, args);
+	}
+}
